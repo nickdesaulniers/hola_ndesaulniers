492fd84a09a5156778041e8f96b6541b8e735f0b
Nick Desaulniers <ndesaulniers@mozilla.com>
2012-08-12 16:45:42 -0700

    added documentation

--- lib/hola_ndesaulniers.rb
+++ lib/hola_ndesaulniers.rb
@@ -1,4 +1,14 @@
+# The main HolaNdesaulniers driver.
 class HolaNdesaulniers
+  # Say hi to the world!
+  #
+  # Example:
+  #   >> HolaNdesaulniers.hi('spanish)
+  #   => Hola Mundo
+  #
+  # Arguments:
+  #   language: (String)
+
   def self.hi language
     translator = Translator.new (language ? language.to_sym : :english)
     translator.hi
